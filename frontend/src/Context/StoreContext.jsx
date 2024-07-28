4740b1259dd7bfe8304e4ef1e4eb454115c79867
RATHOD KAUSHAL <rathodkaushal2001@gmail.com>
2024-07-28 11:21:28 +0530

    login signup cartdata commit

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -1,24 +1,33 @@
+import axios from 'axios';
 import React, { createContext, useEffect, useState } from 'react'
-import { food_list } from '../assets/assets'
 
 export const StoreContext = createContext(null)
 
  const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({})
+    const [token,setToken] = useState("");
+    const url = "http://localhost:4000"
+    const [food_list,setFood_list] = useState([])
 
-    const addToCart = (itemId) => {
+    const addToCart = async (itemId) => {
         if(!cartItems[itemId]) {
             setCartItems((prev) => ({...prev,[itemId]:1}))
         }
         else{
             setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
         }
+
+        if(token) {
+            await axios.post(url+"/api/cart/add",{itemId},{headers:{token}})
+        }
     }
 
-    const removeFromCart = (itemId) => {
+    const removeFromCart = async (itemId) => {
         setCartItems((prev) => ({...prev,[itemId]:prev[itemId]-1}))
-
+        if(token) {
+            await axios.post(url+"/api/cart/remove",{itemId},{headers:{token}})
+        }
     }
 
 
@@ -34,6 +43,28 @@ export const StoreContext = createContext(null)
         return totalAmount;
     }
 
+     const fetchFoodList = async () => {
+        const response = await axios.get(url+"/api/food/list")
+        setFood_list(response.data.data)
+     }
+
+     const loadCartData = async (token) => {
+        const res = await axios.post(url+"/api/cart/get",{},{headers:{token}})
+        setCartItems(res.data.cartData)
+     }
+
+     useEffect(() => {
+       
+        async function loadData() {
+            await fetchFoodList();
+            if (localStorage.getItem("token")) {
+                setToken(localStorage.getItem("token"))
+                await loadCartData(localStorage.getItem("token"));
+            }
+        }
+        loadData();
+    },[])
+
    
 
     const contextvalue = {
@@ -42,7 +73,10 @@ export const StoreContext = createContext(null)
         setCartItems,
         addToCart,
         removeFromCart,
-        getTotalCartAmount
+        getTotalCartAmount,
+        url,
+        token,
+        setToken
     }
 
     return (
